4c24b27304cbbbcb41b3fc526405d0688b9c23ff
Markus Becker <mtib.becker@gmail.com>
2020-10-05 13:24:28 +0200

    Saver local execution script

--- serve_locally.sh
+++ serve_locally.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
-mv _config.yml _config.bak.yml
-sed 's/remote_theme: riggraz\/no-style-please/theme: no-style-please/' _config.bak.yml > _config.yml
-jekyll serve
-rm _config.yml
-mv _config.bak.yml _config.yml
+LOCK="$PWD/.jekyll_lock"
+
+(
+    flock -n 9 || exit 1
+    mv _config.yml _config.bak.yml
+    sed 's/remote_theme: riggraz\/no-style-please/theme: no-style-please/' _config.bak.yml > _config.yml
+    jekyll serve
+    rm _config.yml
+    mv _config.bak.yml _config.yml
+) 9>$LOCK
